e023f7177fc471f36e23ddd1df6d3ffb2fa91c94
Luis Eduardo <luiseduardo250305@gmail.com>
2025-04-24 12:55:49 -0300

    criando componentes e página notfound

---
+++ teste/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/header"
+
+export default function Home(){
+    return(   
+        <div>
+          <Header/>
+          <h2>Olá mundo</h2>
+        </div>
+
+    )
+
+}
---
+++ teste/src/components/header/index.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+    return (
+        <header>
+            <h2>Header do projeto</h2>
+        </header>
+    )
+
+}
