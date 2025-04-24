86ce16efe22370aeb3b59749d1d6bf9512f4b4fc
Luis Eduardo <luiseduardo250305@gmail.com>
2025-04-24 07:44:24 -0300

    estrutura do projet criada

---
+++ teste/Components/header.jsx
@@ -0,0 +1,15 @@
+export default function Header(){
+
+    return(
+
+        <header>
+            <h1>Minha aplicação </h1>
+            <nav>
+                <a href="/">Início</a>
+                <a href="/about">Sobre</a>
+            </nav>
+        </header>
+
+    )
+
+}
---
+++ teste/app/About/page.tsx
@@ -0,0 +1,11 @@
+export default function About(){
+
+    return(
+
+        <main>
+            <h1>Essa é a página sobre</h1>
+        </main>
+
+    )
+
+}
