ae68514bd3d8c47446467eb5f71960333ecc7f05
Luis Eduardo <luiseduardo250305@gmail.com>
2025-04-24 13:05:44 -0300

    Adicionando as metadatas

--- teste/src/app/page.tsx
+++ teste/src/app/page.tsx
@@ -1,12 +1,34 @@
 import { Header } from "@/components/header"
+import { Metadata } from "next"
 
-export default function Home(){
-    return(   
-        <div>
-          <Header/>
-          <h2>Olá mundo</h2>
-        </div>
+export const metadata: Metadata = {
 
-    )
+  title: 'Home - Teste NextJs',
+  description: "Aprendendo next Js",
+  openGraph: {
+    title: "Testando NextJs",
+    description: "Aprendendo NextJs e testando ao mesmo tempo",
+    images: ['']
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true
+    }
+  }
+
+}
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h2>Olá mundo</h2>
+    </div>
+
+  )
 
 }
