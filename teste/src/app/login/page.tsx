8d5223b4eff0f0ba0067cfcf8f4aba416d66d8e2
Luis Eduardo <luiseduardo250305@gmail.com>
2025-04-28 08:43:41 -0300

    Criando a página de login

--- teste/src/app/login/page.tsx
+++ teste/src/app/login/page.tsx
@@ -1,18 +1,19 @@
-export default function Login() {
+export default function Login({ erroLogin = '' }) {
 
     return (
 
 
         <div className="h-screen flex justify-center items-center">
+            {erroLogin && <p style={{ color: 'red' }}>{erroLogin}</p>} {/*Mensagem de erro*/}
             <main className="bg-black w-7xl h-160 text-white rounded-2xl">
                 <h1 className="font-bold text-6xl p-10 pt-25">FAÇA SEU LOGIN</h1>
-                <form action="">
+                <form method="POST" action="/api/login">
                     <ul className="p-10">
                         <li className="font-extralight"><label className="text-2xl " htmlFor="">Email</label><br />
-                            <input type="email" placeholder="Digite seu email" /></li>
+                            <input type="email" name="email" required className="w-72" placeholder="Digite seu email" /></li>
                         <li>
                             <label htmlFor="" className="text-2xl">Senha</label><br />
-                            <input type="password" placeholder="Digite a sua senha" /></li>
+                            <input type="password" name="senha" required className="w-72" placeholder="Digite a sua senha" /></li>
                     </ul>
                 </form>
             </main>
