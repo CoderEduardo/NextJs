fdb252a6b22a687e2f9af8a1704cb2fcc7a0a2f6
Luis Eduardo <luiseduardo250305@gmail.com>
2025-08-25 13:12:32 -0300

    usando ISR, incremental static regeneration, atualizando os dados a cada 60 segundos

---
+++ teste/src/app/isr/page.tsx
@@ -0,0 +1,23 @@
+export const revalidate = 60
+
+export default async function PostISR() {
+
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await res.json()
+
+    return(
+
+        <div>
+            <h1>Posts</h1>
+            <ul>
+                {posts.map((p:any)=>(
+                    <li key={p.id}>{p.title}</li>
+                ))}
+            </ul>
+        </div>
+
+
+    )
+
+
+}
